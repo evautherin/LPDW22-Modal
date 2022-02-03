bbbf3653935ac1cfcabd95cd7fa510940e28a947
Etienne Vautherin <etienne@vautherin.com>
2022-02-03 16:03:13 +0100

    Initial Commit

---
+++ Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Etienne Vautherin on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
